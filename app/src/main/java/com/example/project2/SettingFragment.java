ac2034a4f5626aa5fb4944ba5554770d5e35e326
Jasmine201 <trinhvanthuong01635217802@gmail.com>
2022-05-28 22:08:28 +0700

    Add edit note fragment, update tools bar, lock icon, setting fragment

--- app/src/main/java/com/example/project2/SettingFragment.java
+++ app/src/main/java/com/example/project2/SettingFragment.java
@@ -7,13 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SettingFragment extends Fragment {
+
+    private View view;
+    private Spinner languageSpinner, fontSnipper, soundSnipper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_setting, container, false);
+        //language spinner
+        languageSpinner = view.findViewById(R.id.settingLanguages);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+        //font spinner
+        fontSnipper = view.findViewById(R.id.settingFonts);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.fonts, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontSnipper.setAdapter(adapter1);
+        //ringtone spinner
+        soundSnipper = view.findViewById(R.id.settingSounds);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.ringtones, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        soundSnipper.setAdapter(adapter2);
+        return view;
     }
 }
